0dc495beb1a9557d3f3751f89dce79a7168cc792
Eugene <zheka.los2016@gmail.com>
2023-04-28 18:53:53 +0300

    1

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,22 @@
-
-
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
 import './app.css';
 
-function App() {
+function App () {
     return(
         <div className="app">
-            <h2>!!!</h2>
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployeesList/>
+            <EmployersAddForm/>
         </div>
     );
 }
